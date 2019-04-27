d7cf1b5fde4029dc0a98a581253e8f4f3925343e
Pavlo Pavlenko <alskarf@gmail.com>
2019-04-27 22:12:00 +0300

    added configs

--- relational_database/config.py
+++ relational_database/config.py
@@ -4,14 +4,14 @@ DATABASE = {
     "database": "cursor_db",
     "user": "cursor",
     "password": "very_secret_password",
-    "port": 5433
+    "port": 5432
 }
 
 TEST_DATABASE = {
     "database": "test_cursor_db",
     "user": "test_cursor",
     "password": "test",
-    "port": 5433
+    "port": 5432
 }
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
