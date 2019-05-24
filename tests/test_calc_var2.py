b995601565a28647a5d40c78cb922dd97f05f808
Pavlo Pavlenko <alskarf@gmail.com>
2019-05-24 14:30:06 +0300

    some practice

---
+++ tests/test_calc_var2.py
@@ -0,0 +1,24 @@
+import calc
+from fractions import Fraction
+import unittest
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        assert calc.add(1, 4) == 5, "Should be 5"
+
+    def test_add_fraction(self):
+        """That function tests sum a list of fraction"""
+        data = [Fraction(1, 4), Fraction(1, 4)]
+        result = calc.add(data[0], data[1])
+        self.assertEqual(result, Fraction(2, 4))
+
+    def test_bad_type(self):
+        data = 'vagina'
+        with self.assertRaises(TypeError):
+            result = calc.add(data, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Ok")
